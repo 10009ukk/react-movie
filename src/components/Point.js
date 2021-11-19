16c76e16719447e63e6f5bbc6274da37ec3ac5a8
10009ukk <jeongmal00@naver.com>
2021-11-19 12:24:16 +0900

    detail movies

---
+++ src/components/Point.js
@@ -0,0 +1,28 @@
+import styles from "./Point.module.css";
+
+function Point({background_image_original, medium_cover_image, url, title_long, rating, runtime, genres, description_full}) {
+    return (
+        <div>
+            <img className={styles.bg} src={background_image_original} />           
+            <div className={styles.show}>
+                <img src={medium_cover_image} />
+                <div className={styles.textbox}>
+                    <h1 className={styles.title}><a href={url} target="_blank">{title_long}</a></h1>
+                    <ul>
+                        <li>Rating - {rating}</li>
+                        <li>Runtime - {runtime}</li>
+                        <li>
+                            Genres
+                            <ul>
+                                {genres.map(genre => <li>{genre}</li>)}
+                            </ul>
+                        </li>
+                    </ul>
+                    <p>{description_full.length > 140 ? `${description_full.slice(0, 140)}...` : description_full}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Point;
--- src/router/Detail.js
+++ src/router/Detail.js
@@ -1,12 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {useParams} from "react-router-dom";
+import Point from "../components/Point";
+import styles from "./Home.module.css";
+
 function Detail() {
     const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [datas, setDatas] = useState({});
+
     useEffect(() => {
         fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         .then(response => response.json())
-        .then(console.log)
+        .then(json => {
+            setDatas(json.data.movie);
+            setLoading(false);
+        })
     }, []);
-    return <h1>Detail</h1>;
+    console.log(datas);
+    return (
+        <div className={styles.container}>
+            {loading ? (
+                <div className={styles.loader}>
+                    <span>Loading...</span>
+                </div>
+            ) : (
+                <div>
+                    <Point 
+                        background_image_original={datas.background_image_original}
+                        medium_cover_image={datas.medium_cover_image}
+                        url={datas.url}
+                        title_long={datas.title_long}
+                        rating={datas.rating}
+                        runtime={datas.runtime}
+                        genres={datas.genres}
+                        description_full={datas.description_full}                
+                    />
+                </div>
+            )}
+        </div>
+    );
   }
   export default Detail;
