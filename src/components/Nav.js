0eb7b40661a1d393feb8cb5094ab22759e375723
10009ukk <jeongmal00@naver.com>
2021-11-23 19:36:10 +0900

    bg and font

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -32,7 +32,7 @@ function Nav() {
     
     return <div>
         <nav onMouseOver={onMouseOver} onMouseOut={onMouseOut} style={scrolling ? 
-            {backgroundColor : "#4d8076",
+            {backgroundColor : "#845EC2",
             boxShadow: "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,rgba(0, 0, 0, 0.3) 0px 8px 16px -8px"}
              : {backgroundColor : "transparent"}} className={styles.container} >
         <div className={styles.title}>
